5c3cc9d2d36a48b5f77e19661e8eb007c5f95b07
FrozenBurrito <f3rm3ndouza@outlook.com>
2021-06-05 14:28:14 -0500

    Fix custom css file url

--- includes/templates/header.php
+++ includes/templates/header.php
@@ -15,7 +15,7 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
   
-  <link href="/8_crud/src/css/index.css" rel="stylesheet">
+  <link href="/src/css/index.css" rel="stylesheet">
 
   <title>Sistema CRUD</title>
 </head>
